335bbc57fbc4d55fd99b5f56239250c0cd92b978
pfunfan <saamuelcalancho@gmail.com>
2026-07-06 23:03:08 -0500

    Separa las funciones de ingresos en ingresos.py

---
+++ ingresos.py
@@ -0,0 +1,29 @@
+# Funcion para registrar ingresos
+def registrar_ingreso(conn, cursor):
+    # Preguntar datos
+    fecha = input("Ingrese fecha: \n")
+    nombre = input("Ingrese nombre: \n")
+    baño = float(input("Ingrese monto del baño: \n"))
+    agua = float(input("Ingrese monto del agua: \n"))
+    papel = float(input("Ingrese monto del papel: \n"))
+    
+    # Tupla para insertar valores a la tabla
+    tupla = (fecha, nombre, baño, agua, papel)
+
+    # Insertar valores
+    cursor.execute("""INSERT INTO Ingresos (fecha, nombre, baño, agua, papel)
+         VALUES (?, ?, ?, ?, ?)""", tupla)
+    
+    # Guardar cambios
+    conn.commit()
+    
+    print("Ingreso guardado correctamente\n")
+
+def mostrar_ingresos(cursor):
+    # Leer datos (Selecciona todos los datos)
+    cursor.execute("SELECT * FROM Ingresos")
+    
+    # Ver resultado
+    registros = cursor.fetchall()
+    for registro in registros:
+        print(registro)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from database import conectar, crear_tablas 
+from ingresos import registrar_ingreso, mostrar_ingresos
 
 # Conectarse a la base de datos
 conn, cursor = conectar()
@@ -6,36 +7,7 @@ conn, cursor = conectar()
 # Crear tabla si no existe
 crear_tablas(cursor)
 
-# Funcion para registrar ingresos
-def registrar_ingreso():
-    # Preguntar datos
-    fecha = input("Ingrese fecha: \n")
-    nombre = input("Ingrese nombre: \n")
-    baño = float(input("Ingrese monto del baño: \n"))
-    agua = float(input("Ingrese monto del agua: \n"))
-    papel = float(input("Ingrese monto del papel: \n"))
-    
-    # Tupla para insertar valores a la tabla
-    tupla = (fecha, nombre, baño, agua, papel)
-
-    # Insertar valores
-    cursor.execute("""INSERT INTO Ingresos (fecha, nombre, baño, agua, papel)
-         VALUES (?, ?, ?, ?, ?)""", tupla)
-    
-    # Guardar cambios
-    conn.commit()
-    
-    print("Ingreso guardado correctamente")
-
-def mostrar_ingresos():
-    # Leer datos (Selecciona todos los datos)
-    cursor.execute("SELECT * FROM Ingresos")
-    
-    # Ver resultado
-    registros = cursor.fetchall()
-    for registro in registros:
-        print(registro)
-
+# Menu de opciones y programa
 while True:
     print("""
     =========== COMEDOR POPULAR ===========
@@ -49,9 +21,9 @@ while True:
 
     match opcion:
         case 1:
-            registrar_ingreso()
+            registrar_ingreso(conn, cursor)
         case 2:
-            mostrar_ingresos()
+            mostrar_ingresos(cursor)
         case 3:
             #Cerrar la conexión
             conn.close()
